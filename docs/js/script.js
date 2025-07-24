2aa7d3569939ffa35610855bd66bbe20cd9646fa
Olivia Drayson <39678174+OliviaDrayson@users.noreply.github.com>
2025-07-24 13:49:10 +0200

    Delete docs directory

--- docs/js/script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('changeColorBtn').addEventListener('click', function () {
-    const container = document.querySelector('.container');
-    container.style.backgroundColor = container.style.backgroundColor === '#fff' ? '#f8f8f8' : '#fff';
-});
